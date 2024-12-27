175ee588bd3f69c1521cd328e8e6cd30f699872d
Arpita <arpitasingh01957@gmail.com>
2024-12-27 22:52:38 +0530

    let add closure function

--- advance.js
+++ advance.js
@@ -5,7 +5,7 @@ function say_hello() {
 setTimeout(() => {
     say_hello();
 }, 4000)
-
+// closure
 function outer() {
     let counter = 16
     return function () {
@@ -28,14 +28,30 @@ setTimeout(() => {
 setTimeout(() => {
     console.log('Task 2 finished');
 }, 2000);
-function fetchDataWithCallback(callback) {
-   
-}
-     
-    setTimeout(() => {
-         const data = 'Fetched data'
-         callback(data);
-     console.log( fetchDataWithCallback(data))
-    }, 2000);
 
+    //clousre
+    const greet = (name) => `Hello, ${name}!`;
+const limitedGreet = rateLimiter(greet, 2000); 
+
+console.log(limitedGreet("Arpita")); 
+setTimeout(() => {
+  console.log(limitedGreet("Mannu")); 
+}, 1000); 
+
+setTimeout(() => {
+  console.log(limitedGreet("Aru")); 
+}, 2500); 
+    function rateLimiter(fn, limit) {
+    let lastCalled = 0;
+    return function(...args) {
+      const now = Date.now();
+      if (now - lastCalled < limit) {
+        return 'Rate limit exceeded';
+      } else {
+        lastCalled = now;
+        return fn(...args);
+      }
+    };
+  }
+  
   
